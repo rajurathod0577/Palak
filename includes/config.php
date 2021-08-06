91d0d8044c23d0df6162ab54b2ca0310bd92b2d9
RajuRathod <rajurathod577@gmail.com>
2021-08-06 21:25:08 +0530

    Updates

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,18 @@
 <?php
+
+//Configuration for locakhost
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
 define('DB_NAME', 'palak');
+
+//Configuration for Hosting
+// define('DB_SERVER','localhost');
+// define('DB_USER','root');
+// define('DB_PASS' ,'');
+// define('DB_NAME', 'palak');
+
+
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 // Check connection
