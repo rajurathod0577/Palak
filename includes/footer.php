cc32478e053a97df5a6d8460eabeaad85818f6d0
RajuRathod <rajurathod577@gmail.com>
2021-08-06 20:43:31 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,114 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+<!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Palak Jewellers</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us"> Nam libero tempore, cum soluta nobis est ses  eligendi optio cumque cum soluta nobis est ses  eligendi optio cumque.</p>
+    
+        <div class="social-icons">
+            
+        <a href="https://wa.link/w3y0xk" class='active' target="_blank"><i class="icon fa fa-whatsapp"></i></a>
+        <a href="#"><i class="icon fa fa-facebook"></i></a>
+        <a href="#"><i class="icon fa fa-instagram"></i></a>
+        <a href="#"><i class="icon fa fa-twitter"></i></a>
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+<!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">opening time</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Monday-Friday:</td><td class="pull-right">08.00 To 18.00</td></tr>
+					<tr><td>Saturday:</td><td class="pull-right">09.00 To 20.00</td></tr>
+					<tr><td>Sunday:</td><td class="pull-right">10.00 To 20.00</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">information</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <a href="https://www.google.com/maps/place/Palak+Jewellers/@19.2176039,72.8288659,15z/data=!4m5!3m4!1s0x0:0xc35c5c71f8b04524!8m2!3d19.2176041!4d72.8288739">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                      </a>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Mumbai, Maharashtra</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <a href="tel:+918454831534">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                     </a>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(+91) 8454831534<br>(+91) 8356947093</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                     <a href="mailto:rajurathod577@gmail.com">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                     </a>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="mailto:rajurathod577@gmail.com">rajurathod577@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
