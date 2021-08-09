2d1672f205d1e2cf335717b1be306671ab8a0ed6
RajuRathod <rajurathod577@gmail.com>
2021-08-09 12:56:40 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 
 //Configuration for locakhost
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'palak');
-
-//Configuration for Hosting
 // define('DB_SERVER','localhost');
 // define('DB_USER','root');
 // define('DB_PASS' ,'');
 // define('DB_NAME', 'palak');
+
+//Configuration for Hosting
+define('DB_SERVER','remotemysql.com');
+define('DB_USER','PrnwHiDRAX');
+define('DB_PASS' ,'ajsu3wWGxJ');
+define('DB_NAME', 'PrnwHiDRAX');
 
 
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
