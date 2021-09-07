b8935a22a7afa735902bda5269372d1085406841
RajuRathod <rajurathod577@gmail.com>
2021-09-07 16:19:23 +0530

    DB Update

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,11 +1,19 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'palak');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+//Configuration for locakhost
+// define('DB_SERVER','localhost');
+// define('DB_USER','root');
+// define('DB_PASS' ,'');
+// define('DB_NAME', 'palak');
+
+//Configuration for Hosting
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USER', 'PrnwHiDRAX');
+define('DB_PASS', 'ajsu3wWGxJ');
+define('DB_NAME', 'PrnwHiDRAX');
+
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 // Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
--- includes/config.php
+++ includes/config.php
@@ -1,23 +1,21 @@
 <?php
 
 //Configuration for locakhost
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'palak');
+// define('DB_SERVER','localhost');
+// define('DB_USER','root');
+// define('DB_PASS' ,'');
+// define('DB_NAME', 'palak');
 
 //Configuration for Hosting
-// define('DB_SERVER','remotemysql.com');
-// define('DB_USER','PrnwHiDRAX');
-// define('DB_PASS' ,'ajsu3wWGxJ');
-// define('DB_NAME', 'PrnwHiDRAX');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USER', 'PrnwHiDRAX');
+define('DB_PASS', 'ajsu3wWGxJ');
+define('DB_NAME', 'PrnwHiDRAX');
 
 
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>
